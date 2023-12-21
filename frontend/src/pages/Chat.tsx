a551132b758f6a51feee0e7e651fa34bc8145fe8
PrivFelixKniest <kniest.felix@gmail.com>
2023-12-21 02:35:12 +0100

    fix chat.tsx

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -94,101 +94,83 @@ function Chat() {
     );
   }
 
-  if (!isLoading && isAuthenticated) {
-    return (
-      <Box
-        sx={{
-          width: "100vw",
-          height: "100vh",
-          position: "relative",
-          display: "flex",
-          flexDirection: "column",
-          color: DARKTEAL,
-        }}
-      >
-        <Box
-          sx={{
-            height: "50px",
-            backgroundColor: LIGHTTEAL,
-            padding: "5px 15px",
-            display: "flex",
-            gap: "10px",
-            justifyContent: "center",
-            borderBottom: `2px solid ${DARKTEAL}`,
-          }}
-        >
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        color: DARKTEAL,
+      }}
+    >
+      {!isLoading && isAuthenticated && (
+        <>
           <Box
             sx={{
-              fontWeight: "bold",
-              fontSize: "23px",
+              height: "50px",
+              backgroundColor: LIGHTTEAL,
+              padding: "5px 15px",
               display: "flex",
-              flexDirection: "column",
+              gap: "10px",
               justifyContent: "center",
-              height: "100%",
+              borderBottom: `2px solid ${DARKTEAL}`,
             }}
           >
-            Waill.
-          </Box>
-          <Box
-            sx={{
-              fontSize: "20px",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              height: "100%",
-            }}
-          >
-            Gain Insights Fast, powered by AI
-          </Box>
-        </Box>
-        <Box
-          sx={{ flexGrow: "1", backgroundColor: LIGHTSAND, display: "flex" }}
-        >
-          <Sidebar />
-          <Box
-            sx={{
-              flexGrow: "1",
-              padding: "20px",
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <Box sx={{ flexGrow: "1" }}>
-              <ChatMessages />
+            <Box
+              sx={{
+                fontWeight: "bold",
+                fontSize: "23px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                height: "100%",
+              }}
+            >
+              Waill.
             </Box>
-            <Box sx={{ height: "200px" }}>
-              <ChatInput />
+            <Box
+              sx={{
+                fontSize: "20px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                height: "100%",
+              }}
+            >
+              Gain Insights Fast, powered by AI
             </Box>
           </Box>
-          <Box sx={{ width: "250px", backgroundColor: SAND, padding: "20px" }}>
-            <Box sx={{ fontSize: "20px" }}>Wells</Box>
+          <Box
+            sx={{ flexGrow: "1", backgroundColor: LIGHTSAND, display: "flex" }}
+          >
+            <Sidebar />
+            <Box
+              sx={{
+                flexGrow: "1",
+                padding: "20px",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Box sx={{ flexGrow: "1" }}>
+                <ChatMessages />
+              </Box>
+              <Box sx={{ height: "200px" }}>
+                <ChatInput />
+              </Box>
+            </Box>
+            <Box
+              sx={{ width: "250px", backgroundColor: SAND, padding: "20px" }}
+            >
+              <Box sx={{ fontSize: "20px" }}>Wells</Box>
+            </Box>
           </Box>
-        </Box>
-      </Box>
-    );
-
-    return (
-      <Box
-        style={{
-          width: "100vw",
-          height: "100vh",
-          position: "relative",
-          backgroundColor: "#fef6ea",
-        }}
-      >
-        <div
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-          }}
-        >
-          Something Went Wrong...
-        </div>
-      </Box>
-    );
-  }
+        </>
+      )}
+    </Box>
+  );
 }
 
 export default Chat;
